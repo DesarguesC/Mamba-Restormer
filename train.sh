9192075d437bd203e5ca491ee622b0f83221c895
DesarguesC <425593160@qq.com>
2024-09-21 16:20:36 +0800

    installed

--- train.sh
+++ train.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 CONFIG=$1
+export RANK=0
+export WORLD_SIZE=2     # the number of the thread to be called
+export MASTER_ADDR=localhost
+export MASTER_PORT=5678
+export LOCAL_RANK=0
+export CUDA_VISIBLE_DEVICES=0
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=4321 basicsr/train.py -opt $CONFIG --launcher pytorch
+
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=4321 basicsr/train.py -opt $CONFIG --launcher pytorch
